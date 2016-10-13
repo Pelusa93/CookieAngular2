c3fd046c02d8a6877111aded9c0d1916797d40de
Pedro Alvarez <Pedro Alvarez>
2016-10-13 00:27:24 -0400

    cookies UCABAS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import {CookieService} from 'angular2-cookie/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public _cookieService:CookieService){}
+
+  getall(key: string){
+       return this._cookieService.get(key);
+   }
+
+  getCookie(key: string){
+    return this._cookieService.get(key);
+  }
+
+  prueba(){
+
+    this._cookieService.putObject("Go!",{"token":"pepita2"});
+    this._cookieService.put("Go2!","pepita2");
+    console.log(this._cookieService.get("Go2!"))
+console.log(this._cookieService.getAll());
+console.log(this._cookieService.getObject("Go!"));
+
+  }
+
   title = 'app works!';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 import { AppComponent } from './app.component';
+import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookmarkComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
